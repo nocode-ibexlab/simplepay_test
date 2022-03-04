6b91197aa87665515030eb9d832c4df2e5aba89c
Heejung Lee <heejung.lee@ibexlab.com>
2022-03-04 14:58:25 +0900

    Test3

--- lib/presentation/pages/Login/LOGIN_00_001.dart
+++ lib/presentation/pages/Login/LOGIN_00_001.dart
@@ -116,7 +116,7 @@ const loginId = 'ID';
 const password = 'Пароль';
 const enterPassword = 'Введите пароль';
 const wrongCredentials = 'Неверный ID или пароль1';
-const enter = 'Enter2';
+const enter = 'Enter3';
 
 class NoInkScrollBehavior extends ScrollBehavior {
   @override
